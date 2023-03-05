9721859694b72e9e9eac7fa5dbcae150a1d9b4ac
Lena05k <lena05k@yandex.ru>
2023-03-05 16:08:48 +0300

    Added page ErrorPage

--- frontend/src/components/ErrorPath.jsx
+++ frontend/src/components/ErrorPath.jsx
@@ -1,19 +1,13 @@
 import { useTranslation } from 'react-i18next';
-import errorImage from '../assets/not-found.svg';
 
 const ErrorPath = () => {
   const { t } = useTranslation();
   return(
     <div className="text-center" id="error-page">
-      <img
-        alt="Страница не найдена"
-        style={{ height: '200px', width: '200px' }}
-        src={errorImage}
-      />
-      <h1 className="h4 text-muted">{t('PageNotFound')}</h1>
+      <h1 className="h4 text-muted">{t('titles.pageNotFound')}</h1>
       <p className="text-muted">
-        {t('YouCanChangeover')}
-        <a href="/">{t('GoToMain')}</a>
+        {t('links.toMainPage_title')}
+        <a href="/">{t('links.toMainPage_text')}</a>
       </p>
     </div>
   );
