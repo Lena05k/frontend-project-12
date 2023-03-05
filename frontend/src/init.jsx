6ca9bf9dee1b8d16d5ff27e6269ac78a66f03710
Lena05k <lena05k@yandex.ru>
2023-03-05 17:59:03 +0300

    Added i18n

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -1,10 +1,24 @@
-import { I18nextProvider } from 'react-i18next';
-import i18n from './i18n.jsx';
+import i18next from "i18next";
+import {I18nextProvider, initReactI18next} from 'react-i18next';
+import React from "react";
 import App from './components/App';
-// import {Provider} from 'react-redux';
-// import store from './slices/index';
+import resources from "./locales";
 
-const Init = () => {
+const Init = async () => {
+  const i18n = i18next.createInstance();
+
+  await i18n
+    .use(initReactI18next)
+    .init({
+      lng: 'ru',
+      debug: false,
+      interpolation: {
+        escapeValue: false,
+      },
+      resources,
+      fallbackLng: 'ru',
+    });
+
   return (
     <I18nextProvider i18n={i18n}>
       <App />
@@ -13,7 +27,8 @@ const Init = () => {
 };
 
 export default Init;
-
+// import {Provider} from 'react-redux';
+// import store from './slices/index';
 // <Provider store={store}>
 //
 // </Provider>
