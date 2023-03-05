2c7ef381b20ae06bff99308dccddd213bab84da1
Lena05k <lena05k@yandex.ru>
2023-03-05 16:11:37 +0300

    Delete Provider

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -1,19 +1,19 @@
+import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n.jsx';
-import {I18nextProvider} from 'react-i18next';
-import {Provider} from 'react-redux';
-import {BrowserRouter} from 'react-router-dom';
 import App from './components/App';
+// import {Provider} from 'react-redux';
+// import store from './slices/index';
 
 const Init = () => {
   return (
     <I18nextProvider i18n={i18n}>
-      <Provider store={store}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </Provider>
+      <App />
     </I18nextProvider>
   );
 };
 
 export default Init;
+
+// <Provider store={store}>
+//
+// </Provider>
