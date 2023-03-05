4d5f819d2fb097f787c570ae6c04a0072dde9cd3
Lena05k <lena05k@yandex.ru>
2023-03-05 16:12:04 +0300

    Added text

--- frontend/src/locales/ru.js
+++ frontend/src/locales/ru.js
@@ -1,1 +1,9 @@
-export default {};
+export default {
+  titles: {
+    pageNotFound: 'Страница не найдена',
+  },
+  links: {
+    toMainPage_title: 'Но вы можете перейти на главную страницу',
+    toMainPage_text: 'на главную страницу',
+  },
+};
