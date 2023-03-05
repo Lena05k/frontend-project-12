97c642b3c16564cde925aeffbc4822ff117fde20
Lena05k <lena05k@yandex.ru>
2023-03-05 17:59:44 +0300

    Added t

--- frontend/src/components/ErrorPath.jsx
+++ frontend/src/components/ErrorPath.jsx
@@ -2,11 +2,13 @@ import { useTranslation } from 'react-i18next';
 
 const ErrorPath = () => {
   const { t } = useTranslation();
+  console.log(t('titles.pageNotFound'));
   return(
     <div className="text-center" id="error-page">
       <h1 className="h4 text-muted">{t('titles.pageNotFound')}</h1>
       <p className="text-muted">
         {t('links.toMainPage_title')}
+        {' '}
         <a href="/">{t('links.toMainPage_text')}</a>
       </p>
     </div>
