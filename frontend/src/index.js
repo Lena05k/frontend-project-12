ed6c26c74eded358f77a61a0356ce70eb59d132e
Lena05k <lena05k@yandex.ru>
2023-05-11 19:45:46 +0300

    Added root.render

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,19 +1,13 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import './i18n';
-import { Provider } from 'react-redux';
-import reportWebVitals from './reportWebVitals';
-import store from './slices/index';
-import App from './components/App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { io } from 'socket.io-client';
+import initChat from './init.jsx';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const app = async () => {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  const socket = io();
+  root.render(await initChat(socket));
+};
 
-root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-);
-
-reportWebVitals();
+app();
