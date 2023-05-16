78b86ef754e5ad6f6ea73cbbae9acdf752316046
Lena05k <lena05k@yandex.ru>
2023-05-16 17:53:21 +0300

    Added token

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -9,16 +9,31 @@ import filter from 'leo-profanity';
 import ru from './locales/ru';
 import { actions as messagesActions } from './slices/messagesSlice';
 import { actions as channelsActions } from './slices/channelsSlice';
-import { SocketContext } from './contexts/index';
+import { ApiContext } from './contexts/index';
 import App from './components/App';
 import store from './slices/index.js';
 import AuthProvider from './contexts/AuthProvider';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const socket = io();
-
-const SocketProvider = ({ children }) => {
+const initChat = async (socket) => {
   const dispatch = useDispatch();
+
+  const rollbarConfig = {
+    accessToken: process.env.REACT_APP_ROLLBAR_ACCESS_TOKEN,
+    environment: 'production',
+  };
+
+  const i18n = i18next.createInstance();
+  await i18n
+    .use(initReactI18next)
+    .init({
+      resources: { ru },
+      lng: 'ru',
+
+      interpolation: {
+        escapeValue: false,
+      },
+    })
 
   socket.on('newMessage', (payload) => {
     dispatch(messagesActions.addMessage(payload));
@@ -36,42 +51,15 @@ const SocketProvider = ({ children }) => {
     dispatch(channelsActions.setChannel(payload));
   });
 
-  return (
-    <SocketContext.Provider value={useMemo(() => ({ socket }), [])}>
-      {children}
-    </SocketContext.Provider>
-  );
-};
-
-const initChat = async (socketOn) => {
   filter.add(filter.getDictionary('ru'));
-
-  const rollbarConfig = {
-    accessToken: '1be4cfe4d0894c0fb892e19a57aff1c4',
-    environment: 'production',
-  };
-
-  const i18n = i18next.createInstance();
-  await i18n
-    .use(initReactI18next)
-    .init({
-      resources: { ru },
-      lng: 'ru',
-
-      interpolation: {
-        escapeValue: false,
-      },
-    });
 
   return (
     <Provider store={store}>
       <RollbarProvider config={rollbarConfig}>
         <ErrorBoundary>
-          <SocketProvider socket={socketOn}>
-            <AuthProvider>
-              <App />
-            </AuthProvider>
-          </SocketProvider>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
           <ToastContainer />
         </ErrorBoundary>
       </RollbarProvider>
