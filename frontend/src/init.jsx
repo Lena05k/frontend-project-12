e9a5b4533262883efa743314b1748fb004ad8033
Lena05k <lena05k@yandex.ru>
2023-05-19 15:26:12 +0300

    Add provider

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -1,23 +1,27 @@
-import { useMemo } from 'react';
-import { Provider, useDispatch } from 'react-redux';
+import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-bootstrap';
 import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
-import { io } from 'socket.io-client';
 import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import filter from 'leo-profanity';
 import ru from './locales/ru';
-import { actions as messagesActions } from './slices/messagesSlice';
-import { actions as channelsActions } from './slices/channelsSlice';
-import { ApiContext } from './contexts/index';
 import App from './components/App';
+import { ApiContext } from './contexts/index';
 import store from './slices/index.js';
 import AuthProvider from './contexts/AuthProvider';
+import socketConfigure from './contexts/socketConfigure';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const initChat = async (socket) => {
-  const dispatch = useDispatch();
+const ApiProvider = ({ socket, children }) => {
+  const api = socketConfigure(socket);
+  return (
+    <ApiContext.Provider value={api}>
+      {children}
+    </ApiContext.Provider>
+  );
+};
 
+const initChat = async (socket) => {
   const rollbarConfig = {
     accessToken: process.env.REACT_APP_ROLLBAR_ACCESS_TOKEN,
     environment: 'production',
@@ -33,23 +37,7 @@ const initChat = async (socket) => {
       interpolation: {
         escapeValue: false,
       },
-    })
-
-  socket.on('newMessage', (payload) => {
-    dispatch(messagesActions.addMessage(payload));
-  });
-
-  socket.on('newChannel', (payload) => {
-    dispatch(channelsActions.addChannel(payload));
-  });
-
-  socket.on('removeChannel', ({ id }) => {
-    dispatch(channelsActions.removeChannel(id));
-  });
-
-  socket.on('renameChannel', (payload) => {
-    dispatch(channelsActions.setChannel(payload));
-  });
+    });
 
   filter.add(filter.getDictionary('ru'));
 
@@ -57,9 +45,11 @@ const initChat = async (socket) => {
     <Provider store={store}>
       <RollbarProvider config={rollbarConfig}>
         <ErrorBoundary>
-          <AuthProvider>
-            <App />
-          </AuthProvider>
+          <ApiProvider socket={socket}>
+            <AuthProvider>
+              <App />
+            </AuthProvider>
+          </ApiProvider>
           <ToastContainer />
         </ErrorBoundary>
       </RollbarProvider>
