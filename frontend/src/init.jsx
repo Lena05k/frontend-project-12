693aceba43d22eeb484d8f0547d216097490fb0f
Lena05k <lena05k@yandex.ru>
2023-05-31 12:12:27 +0300

    ...

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -1,4 +1,5 @@
 import { Provider } from 'react-redux';
+import { useMemo } from 'react';
 import { ToastContainer } from 'react-bootstrap';
 import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 import i18next from 'i18next';
@@ -8,8 +9,39 @@ import ru from './locales/ru';
 import App from './components/App';
 import store from './slices/index.js';
 import AuthProvider from './contexts/AuthProvider';
-import InitSocket from './contexts/initSocket';
+import { ApiContext } from './contexts/index';
+import { actions as messagesActions } from './slices/messagesSlice';
+import { actions as channelsActions } from './slices/channelsSlice';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+const promosifySocket = (socket, type, data) => new Promise((resolve, reject) => {
+  socket.emit(type, data, (err, response) => {
+    if (err) {
+      reject(err);
+    }
+    resolve(response);
+  });
+});
+
+const InitSocket = ({ socket, children }) => {
+  const sendMessage = (message) => promosifySocket(socket, 'newMessage', message);
+  const createChannel = (channel) => promosifySocket(socket, 'newChannel', channel);
+  const removeChannel = (channelId) => promosifySocket(socket, 'removeChannel', channelId);
+  const renameChannel = (channel) => promosifySocket(socket, 'renameChannel', channel);
+
+  const api = useMemo(() => ({
+    sendMessage,
+    createChannel,
+    removeChannel,
+    renameChannel,
+  }), [sendMessage, createChannel, removeChannel, renameChannel]);
+
+  return (
+    <ApiContext.Provider value={api}>
+      {children}
+    </ApiContext.Provider>
+  );
+};
 
 const initChat = async (socket) => {
   const rollbarConfig = {
@@ -29,17 +61,33 @@ const initChat = async (socket) => {
       },
     });
 
+  socket.on('newMessage', (payload) => {
+    store.dispatch(messagesActions.addMessage(payload));
+  });
+
+  socket.on('newChannel', (payload) => {
+    store.dispatch(channelsActions.addChannel(payload));
+  });
+
+  socket.on('removeChannel', ({ id }) => {
+    store.dispatch(channelsActions.removeChannel(id));
+  });
+
+  socket.on('renameChannel', (payload) => {
+    store.dispatch(channelsActions.setChannel(payload));
+  });
+
   filter.add(filter.getDictionary('ru'));
 
   return (
     <RollbarProvider config={rollbarConfig}>
       <ErrorBoundary>
         <Provider store={store}>
-          {/*<InitSocket socket={socket}>*/}
+          <InitSocket socket={socket}>
             <AuthProvider>
               <App />
             </AuthProvider>
-          {/*</InitSocket>*/}
+          </InitSocket>
         </Provider>
         <ToastContainer />
       </ErrorBoundary>
