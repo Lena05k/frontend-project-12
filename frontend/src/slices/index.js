22bddaf7a03110b1c9886c67517936636f2023be
Lena05k <lena05k@yandex.ru>
2023-06-05 13:29:53 +0300

    Add modalsSlice

--- frontend/src/slices/index.js
+++ frontend/src/slices/index.js
@@ -1,12 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import channelsReducer from './channelsSlice';
 import messagesReducer from './messagesSlice';
-import modalReducer from './modalSlice';
+import modalsSlice from './modalSlice';
 
 export default configureStore({
   reducer: {
     channels: channelsReducer,
     messages: messagesReducer,
-    modal: modalReducer,
+    modalsSlice,
   },
 });
