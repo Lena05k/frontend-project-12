6805b1bc20bad13e4485fd6a794482a4c1499680
Lena05k <lena05k@yandex.ru>
2023-06-05 13:27:24 +0300

    Added modal

---
+++ frontend/src/components/Modal.jsx
@@ -0,0 +1,21 @@
+import { useSelector } from 'react-redux';
+import { Modal as ModalComponent } from 'react-bootstrap';
+import getModal from './modals/index';
+
+const Modal = () => {
+  const { type } = useSelector((state) => state.modalsSlice);
+  const modalShow = !!type;
+
+  if (!type) {
+    return null;
+  }
+  const Component = getModal(type);
+  console.log('type:', type);
+
+  return (
+    <ModalComponent show={modalShow} centered>
+      <Component />
+    </ModalComponent>
+  );
+};
+export default Modal;
